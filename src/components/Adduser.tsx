3a3320ea22674922362b5f2619741a312f1926b0
sam1code <samcse95@gmail.com>
2021-09-30 00:20:14 +0530

    Data addeds

--- src/components/Adduser.tsx
+++ src/components/Adduser.tsx
@@ -5,7 +5,10 @@ const Adduser = ({ onAdd }) => {
     e.preventDefault();
     onAdd(e.target.name.value, e.target.email.value);
     e.target.name.value = "";
+    e.target.username.value = "";
     e.target.email.value = "";
+    e.target.phone.value = "";
+    e.target.website.value = "";
   };
 
   return (
@@ -13,7 +16,10 @@ const Adduser = ({ onAdd }) => {
       <form onSubmit={handleOnSubmit}>
         <h3>Add User</h3>
         <input placeholder="Name" name="name" />
+        <input placeholder="UserName" name="username" />
         <input placeholder="Email" name="email" />
+        <input placeholder="phone" name="phone" />
+        <input placeholder="website" name="website" />
         <button onSubmit={handleOnSubmit}>Add</button>
         <hr />
       </form>
